94140677b729fe8e867eba6f160263710a3e9ec4
Folusho Oladipo <folusho.oladipo@andela.com>
2018-01-29 20:10:46 +0100

    enable /book/:id to show a specified book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookProfileComponent } from './book-profile/book-profile.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'book/:id', component: BookProfileComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
 ];
 
--- src/app/book-profile/book-profile.component.ts
+++ src/app/book-profile/book-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { BooksService } from '../books.service';
+import { Book } from '../book';
 
 @Component({
   selector: 'app-book-profile',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-profile.component.css']
 })
 export class BookProfileComponent implements OnInit {
-
-  constructor() { }
+  book: Book;
+  constructor(
+    private bookService: BooksService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.getBook();
   }
 
+  // TODO: Add more elegant error handling here.
+  getBook() {
+    const bookId = Number(this.route.snapshot.paramMap.get('id'));
+    console.log('bookId', bookId);
+    if (Number.isNaN(bookId)) {
+      this.book = {
+        id: -1,
+        name: 'ERROR! Invalid book id!',
+        author: 'Error',
+        yearOfPublication: 'Error',
+        genres: []
+      };
+      return;
+    }
+
+    this.book = this.bookService.getBook(bookId);
+  }
 }
