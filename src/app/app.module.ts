dd280aedd1d85f99c23fdb36ee3e7170a92f95ea
Folusho Oladipo <folusho.oladipo@andela.com>
2018-01-29 13:11:27 +0100

    create BookDetail.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
